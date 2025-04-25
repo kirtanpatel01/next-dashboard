f944b14e9d166e9d411bdb3f4be2620ea4d5e5f8
kirtan0101 <kjpatel200022@gmail.com>
2025-04-25 15:59:16 +0530

    feat: add error handling and not found page for invoice editing

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,16 +1,21 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
- 
-export default async function Page(props: { params: Promise<{ id: string }>}
-) {
-    const params = await props.params;
-    const id = params.id;
+import { notFound } from 'next/navigation';
 
-    const [ invoice, customers ] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-    ])
+export default async function Page(props: { params: Promise<{ id: string }> }
+) {
+  const params = await props.params;
+  const id = params.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ])
+
+  if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
